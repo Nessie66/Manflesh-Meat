8719a0b28636d468082e8aa3a71b8783c83b5c8b
nessie66 <tristyn@tilster.com>
2024-02-19 18:51:25 +0000

    div'd svg and first speech box

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,3 @@
-//setting up speech 1
-function speech1(speech) {
-    var text = document.createElement("h1");
-    text.innerHTML = speech;
-    document.body.append(text);
-    console.log("edited")
-}
-
 //reload page on back
 (function () {
 	window.onpageshow = function(event) {
@@ -15,14 +7,21 @@ function speech1(speech) {
 	};
 })();
 
+function speech(message){
+    let div = document.getElementsByClassName("speech")
+    let text = div[0].getElementsByTagName("p")
+    text[0].innerHTML = message;
+    div[0].classList.add("show")
+    console.log("working")
+}
+
 //potentialPits visited
 window.onload = function () {
-    console.log(localStorage.getItem("potentialPits"))
     if (localStorage.getItem("potentialPits") == null){
         console.log("initial set")
     }
     if (localStorage.getItem("potentialPits") == 1){
-        console.log("ready")
-        speech1("test");
+        speech("i wonder whats down there");
+
     }
 }
