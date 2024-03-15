621b14a7cf4cad9c7ec3795896b30b732d6bd628
nessie66 <tristyn@tilster.com>
2024-03-15 17:45:35 +0000

    fixes and stuff

---
+++ cont/sec1/scripts/hallway2.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    let link = document.getElementById("click")
+    if(localStorage.getItem("hallway3") == 1){
+        link.classList.add("show")
+        link.addEventListener("click", function() {
+            console.log("clicked")
+            localStorage.setItem("clickme", 1)  
+        });
+        if(localStorage.getItem("clickme") == 1){
+            link.classList.remove("show")
+        }
+    }
+}
