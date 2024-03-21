67db49d647a73abe8d447a0632096a43bb90ee0c
nessie66 <tristyn@tilster.com>
2024-03-21 02:41:57 +0000

    tweaks and changes

--- cont/sec1/scripts/dead_end1.js
+++ cont/sec1/scripts/dead_end1.js
@@ -61,7 +61,7 @@ window.onload = function () {
             talk.classList.add("killyourselfcss");
             changeImage(event1);
             button1.classList.remove("show");
-            speech("So be it... I hear she lurks around the potential pits, if you can make it that far. I'd be wary of venturing any deeper, horrid things lurk down there. Muligastric above and multigastric below you see. We're trapped.")
+            speech("So be it... I hear she lurks around the potential pits, if you can make it that far.<br> I'd be wary of venturing any deeper. Horrid things lurk down there. Muligastric above and multigastric below you see. We're trapped.")
             localStorage.setItem("dead_end1", 1)  
         });
     }
