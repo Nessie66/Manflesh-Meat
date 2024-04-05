afe0f662fcde9d633e151c40a82b5cf408e37326
nessie66 <tristyn@tilster.com>
2024-04-05 21:02:29 +0100

    loading fixed + delay

--- cont/sec1/scripts/dead_end1.js
+++ cont/sec1/scripts/dead_end1.js
@@ -27,7 +27,6 @@ function showSpeech() {
     let div = document.getElementsByClassName("speech")
     let text = div[0].getElementsByTagName("p")
     div[0].classList.add("show")
-    console.log("speech")
     // let svg = document.getElementsByTagName("svg");
     // svg[0].classList.add("show")
 }
@@ -55,17 +54,18 @@ if((localStorage.getItem("DEQ") == 1)){
     speech("Argh! You scared me, what are you doing here?<br><br>Girlwolf decade destroyer?<br>Where did you hear about that? Why do you want to know?")  
 }
 
-if(localStorage.getItem("dead_end1") == null){     
-    showSpeech()
-    button1.classList.add("show");
+if(localStorage.getItem("dead_end1") == null){   
+    setTimeout(function(){ showSpeech(); }, 500); 
+    setTimeout(function(){ changeImage(event1); }, 500);  
+    setTimeout(function(){  button1.classList.add("show");; }, 500); 
 }
 
 window.onload = function () {
     if(localStorage.getItem("dead_end1") == null){     
-        
+
         button1.addEventListener("click", function() {
             talk.classList.add("killyourselfcss");
-            changeImage(event1);
+            // changeImage(event1);
             button1.classList.remove("show");
             speech("So be it... I hear she lurks around the potential pits, if you can make it that far.<br> I'd be wary of venturing any deeper. Horrid things lurk down there. Muligastric above and multigastric below you see. We're trapped.")
             localStorage.setItem("dead_end1", 1)  
