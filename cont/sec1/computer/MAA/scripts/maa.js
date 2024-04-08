a21eef1d92890b7389a36a68d41ff4056b3486bf
nessie66 <tristyn@tilster.com>
2024-04-08 18:22:25 +0100

    temp downloads page

--- cont/sec1/computer/MAA/scripts/maa.js
+++ cont/sec1/computer/MAA/scripts/maa.js
@@ -9,4 +9,8 @@ function eyeball() {
     drag.outerHTML = '<img src="img/handsforballs.gif">';
     let head1 = document.getElementsByTagName("h1")[0];
     head1.innerHTML = 'Monogastric Eyeball Arousal';
+    let poem = document.getElementsByClassName("poem")[0];
+    poem.innerHTML = '"i dont know man. just eyeball it"';
+    let poet = document.getElementsByClassName("poem")[1];
+    poet.innerHTML = 'why you askin me?';
 }
