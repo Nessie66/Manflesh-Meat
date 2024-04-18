20fe1adae80e0491c5668a0e2d73352fb0d76a0c
nessie66 <tristyn@tilster.com>
2024-04-18 23:41:55 +0100

    icm js changes

--- cont/sec1/scripts/icm.js
+++ cont/sec1/scripts/icm.js
@@ -15,46 +15,50 @@ window.onload = function() {
     });
 };
 
-const num = 260;
-const phraseBuffer = 25; 
-
-var usedPhrases = [];
+const num = 250; //250
+const phrasesBuffer = 50; //50
 
 function displayRandomPhrases(phrases, numberOfPhrases) {
     var phraseContainer = document.getElementById('phrase-container');
     phraseContainer.innerHTML = '';
 
-    // Initialize a counter object to keep track of phrase occurrences
-    var phraseCounters = {};
-
-    for (var i = 0; i < phrases.length; i++) {
-        phraseCounters[phrases[i]] = 0;
-    }
-
     var selectedPhrases = [];
+    var recentlySelected = new Set();
+
     while (selectedPhrases.length < numberOfPhrases) {
         phrases.sort(function() {
             return Math.random() - 0.5;
         });
 
         // Select a random phrase
-        var randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-        
-        // Check if the phrase can be displayed based on its counter
-        if (phraseCounters[randomPhrase] < 10) { // Change 10 to your desired limit
+        var randomIndex = Math.floor(Math.random() * phrases.length);
+        var randomPhrase = phrases[randomIndex];
+
+        // Check if the phrase was recently selected
+        if (!recentlySelected.has(randomPhrase)) {
+            // Add the phrase to the selected list
             selectedPhrases.push(randomPhrase);
-            phraseCounters[randomPhrase]++;
+            
+            // Add the phrase to the recently selected set
+            recentlySelected.add(randomPhrase);
+
+            // Remove the oldest phrase from the buffer if it exceeds the buffer size
+            if (recentlySelected.size > phrasesBuffer) {
+                var oldestPhrase = selectedPhrases[selectedPhrases.length - phrasesBuffer - 1];
+                recentlySelected.delete(oldestPhrase);
+            }
         }
+
+        
     }
 
     var allPhrases = selectedPhrases.join(' ');
 
-    // Inserting <a> tag at a random position in the paragraph
     var randomPosition = Math.floor(Math.random() * allPhrases.length);
     allPhrases = allPhrases.slice(0, randomPosition) + '<a class="link" href="immitation_crab_meat.html">&nbspHEY MAN SHUTUP YOURE JUST SPOUTING EMPTY WORDS AND RHYMES!&nbsp</a>' + allPhrases.slice(randomPosition);
 
     var paragraph = document.createElement('p');
-    paragraph.innerHTML = allPhrases; // Use innerHTML to insert HTML tags
+    paragraph.textContent = allPhrases;
+    paragraph.innerHTML = allPhrases; 
     phraseContainer.appendChild(paragraph);
 }
-
