5e35f89e32de738e42e9d022aefd6b8e4ea036d3
nessie66 <tristyn@tilster.com>
2025-02-11 20:09:58 +0000

    puzzleroy V3

--- caves/depths_1/megaroy/abominations/scripts/abomination.js
+++ caves/depths_1/megaroy/abominations/scripts/abomination.js
@@ -1,11 +1,15 @@
 document.getElementById('b1').addEventListener('click', function() {
 
+    var breathingSound = document.getElementById('b');
+    breathingSound.src = ''; 
+
     var sound1 = document.getElementById('sound1');
-    sound1.play(); 
+    
     var abomination = document.getElementById('abomination');
-    abomination.src = 'img/puddle.png';  
+    abomination.src = 'img/pixelpuddle.png';  
 
     abomination.onload = function() { 
+        sound1.play(); 
         abomination.classList.add('puddle');
     };
 
@@ -22,24 +26,23 @@ document.getElementById('b1').addEventListener('click', function() {
 
 document.getElementById('b2').addEventListener('click', function() {
 
+    var breathingSound = document.getElementById('b');
+    breathingSound.src = ''; 
+
+
     var sound2 = document.getElementById('sound2');
     sound2.play();
-
 
     var abomination = document.getElementById('abomination');
     abomination.style.transition = 'opacity 3s ease-out';
     abomination.style.opacity = '0'; 
 
-
     document.querySelector('.free').style.display = 'block';
     document.querySelector('.default').style.display = 'none';
 
-
     document.querySelector('.misery').style.display = 'none';
 
-
     document.querySelector('.select').style.display = 'none';
-
 
     setTimeout(function() {
         window.location.href = '../puzzleroy.html';  
